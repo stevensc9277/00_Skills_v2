342b904313fdf968fc319e3db1a6a7ddee96a108
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-02-05 13:07:43 +1300

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+# get input
+
+# ask user for name
+name = input("What is your name? ")
+
+# ask user for two numbers
+
+num_1 = int(input("Pick a number from one to ten. "))
+if num_1 == 10:
+    print("you chose ten")
+else:
+    print("you did not choose 10")
+
+
+# add two numbers together
+
+# multiply two numbers together
+
+# greet user and display math
